34c73930f79356ff270a8a9e1cfda6e3cb6aac67
georgiagibbison <georgia.gibbison@ada.ac.uk>
2025-10-07 10:50:35 +0000

    kilometers to miles

--- 1_temperature_converter.py
+++ 1_temperature_converter.py
@@ -1,8 +1,23 @@
 def celsius_to_fahrenheit(celsius):
     fahrenheit = (celsius * 9/5) + 32
     return fahrenheit
-
-# Test the function
+def fahrenheit_to_celsius(fahrenheit):
+    celsius= (fahrenheit/9*5)-32
+    return celsius
+# Test the c to f function
 print(celsius_to_fahrenheit(0))
 print(celsius_to_fahrenheit(20))
 print(celsius_to_fahrenheit(100))
+print(celsius_to_fahrenheit(-40))
+# Test the f to c function 
+print(fahrenheit_to_celsius(0))
+print(fahrenheit_to_celsius(20))
+print(fahrenheit_to_celsius(100))
+print(fahrenheit_to_celsius(-40))
+
+
+def kilometers_to_miles(kilometers):
+    miles=kilometers*0.621371
+    return miles
+print (kilometers_to_miles(1))
+print(kilometers_to_miles(5))
